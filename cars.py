5e301f409edb52ab4d464a225d5817da2fe7b542
Jakob Wells <jakobwithak@gmail.com>
2022-02-09 14:15:03 +0800

    Sort a list temporarily with the sorted() function

--- cars.py
+++ cars.py
@@ -1,3 +1,10 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort(reverse=True)
+
+print('Here is the original list:')
+print(cars)
+
+print('\nHere is the sorted list:')
+print(sorted(cars))
+
+print('\nHere is the original list again:')
 print(cars)
