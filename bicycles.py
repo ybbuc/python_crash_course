b5294cb1a31ad12352640c037fefdcd5c7a032e7
Jakob Wells <jakobwithak@gmail.com>
2022-02-09 11:20:31 +0800

    Create a message based on a value from a list

--- bicycles.py
+++ bicycles.py
@@ -1,5 +1,3 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-print(bicycles[0].title())
-print(bicycles[1].title())
-print(bicycles[3].title())
-print(bicycles[-1].title())
+message = f"My first bicycle was a {bicycles[0].title()}."
+print(message)
