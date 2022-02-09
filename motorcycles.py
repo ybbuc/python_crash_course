874bad2ce78a1d09ac2d4629eeb6897e64f2c5b7
Jakob Wells <jakobwithak@gmail.com>
2022-02-09 13:31:34 +0800

    Pop the first item from a list

--- motorcycles.py
+++ motorcycles.py
@@ -1,4 +1,4 @@
 motorcycles = ['honda', 'yamaha', 'suzuki']
 
-last_owned = motorcycles.pop()
-print(f"The last motorcycle I owned was a {last_owned.title()}.")
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
