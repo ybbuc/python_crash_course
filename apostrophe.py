ee7fd1e40a41a9fd3ebfb55f31b11006eda246be
Jakob Wells <jakobwithak@gmail.com>
2022-02-09 10:43:11 +0800

    Test apostrophes in strings

---
+++ apostrophe.py
@@ -0,0 +1,6 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+# If you want to use an apostrophe in a string, you need to use double quotes
+message = 'One of Python's strengths is its diverse community.'
+print(message)
