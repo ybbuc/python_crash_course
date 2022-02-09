b0ef930eba81c77c2582f78a4e66d8144203068e
Jakob Wells <jakobwithak@gmail.com>
2022-02-09 13:34:56 +0800

    Remove an item from a list by value

--- motorcycles.py
+++ motorcycles.py
@@ -1,4 +1,7 @@
-motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+print(motorcycles)
 
-first_owned = motorcycles.pop(0)
-print(f"The first motorcycle I owned was a {first_owned.title()}.")
+too_expensive = 'ducati'
+motorcycles.remove(too_expensive)
+print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
