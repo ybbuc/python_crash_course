6adf77e13064f1253821591c5906705a5befe8e2
Jakob Wells <jakobwithak@gmail.com>
2022-02-10 15:45:16 +0800

    Add looping through list exercises

---
+++ animals.py
@@ -0,0 +1,5 @@
+animals = ['cat', 'dog', 'monkey']
+for animal in animals:
+    print('A ' + animal + ' would make a great pet.')
+
+print('\nAny of these animals would make a great pet!')
---
+++ pizzas.py
@@ -0,0 +1,5 @@
+pizzas = ['pepperoni', 'sausage', 'cheese', 'veggie']
+for pizza in pizzas:
+    print(f"I like {pizza} pizza.")
+
+print("\nI really love pizza!")
