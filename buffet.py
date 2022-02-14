468af38d4d84f4ea8d4c6d02a635ccb0a01f6441
Jakob Wells <jakobwithak@gmail.com>
2022-02-14 13:32:46 +0800

    Add tuple exercise

---
+++ buffet.py
@@ -0,0 +1,11 @@
+foods = ('burger', 'pizza', 'salad', 'fries', 'soup')
+
+print('Original menu:')
+for food in foods:
+    print(food)
+
+foods = ('fried chicken', 'pizza', 'salad', 'sandwich', 'soup')
+
+print('\nModified menu:')
+for food in foods:
+    print(food)
