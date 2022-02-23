3f42bfd007c23fe9722810c322525633786702f6
Jakob Wells <jakob@duck.com>
2022-02-23 17:23:28 +0800

    Refactor to use a function

--- greeter.py
+++ greeter.py
@@ -1,4 +1,9 @@
+def greet_user(username):
+    """Display a simple greeting"""
+    print(f"\nHello, {username}!")
+
 prompt = "If you tell us who you are, we can personalize the messages you see."
 prompt += "\nWhat is your first name? "
 name = input(prompt)
-print(f"\nHello, {name}!")
+
+greet_user(name)
