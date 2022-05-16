98ae5a975e29029190a7144e0ff57cc1d2705769
Jakob Wells <jakobwithak@gmail.com>
2022-05-16 13:48:22 +0800

    Handling the ZeroDivisionError exception

---
+++ division_calculator.py
@@ -0,0 +1,16 @@
+print("Give me two numbres, and I'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\n First number: ")
+    if first_number.lower() == 'q':
+        break
+    second_number = input("Second number: ")
+    if second_number.lower() == 'q':
+        break
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by zero!")
+    else:
+        print("       Answer:", answer)
